edd3820b2097c7a3403c9833c10a20d8bc3d4a8f
Adrian Carmiol <acarmiolu@ucenfotec.ac.cr>
2017-03-08 15:15:53 -0600

    updating my login page with nodejs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Handle Sessions
 app.use(session({
 	secret:'secret',
-	saveUnitialized:true,
+	saveUninitialized:true,
 	resave:true
 }));
 
@@ -67,6 +67,10 @@ app.use(expressValidator({
 }));
 
 //express messages
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
@@ -75,7 +79,7 @@ app.use(function (req, res, next) {
 
 
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -86,14 +90,23 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+if(app.get('env') === 'development'){
+  app.use(function(err, req, res, next){
+    res.status(err.status ||500);
+    res.render('error', {
+      message: err.message,
+      error:err
+    })
+  })
+}
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error:{}
+  });
 });
 
 module.exports = app;
